1e3a82ab182bde5faf34b0620790a24be88be807
kmtusher97 <kmtusher97@gmail.com>
2021-02-19 16:55:10 +0600

    Implemented dynamic update of target category name and image

--- web_app/web_app/views.py
+++ web_app/web_app/views.py
@@ -5,4 +5,4 @@ from .captcha_generator import create_new_captcha
 def home_page(request):
     data = create_new_captcha()
     print(data)
-    return render(request, "index.html")
+    return render(request, "index.html", data)
