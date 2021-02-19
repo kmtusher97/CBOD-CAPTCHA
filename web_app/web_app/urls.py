a30db7c5f87e66e4cac2ecaed128e529ea1013b4
kmtusher97 <kmtusher97@gmail.com>
2021-02-19 16:18:40 +0600

    added static files management created url for home added templete location

--- web_app/web_app/urls.py
+++ web_app/web_app/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .views import home_page
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    path('', home_page),
+    path('admin/', admin.site.urls)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
